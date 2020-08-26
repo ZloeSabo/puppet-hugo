e1d678226931496d1e37f17ef644ba84846d8ad6
Evgeny Soynov <saboteur@saboteur.me>
2020-08-26 21:07:06 +0200

    Converting old module to work with PDK

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,39 +1,50 @@
-require 'beaker-rspec'
+#require 'beaker-rspec'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-options = {
-  default_action: 'gem_install'
-}
-
-block_on hosts, run_in_parallel: false do |host|
-  on host, 'pkg install -y devel/ruby-gems' if host['platform'] =~ %r{freebsd}
-  if host['platform'] =~ %r{openbsd}
-    version = ''
-    if host['platform'] =~ %r{openbsd-6}
-      on host, '. /root/.profile && pkg_add ruby-2.3.1p1'
-      version = '23'
-    end
-
+configure_beaker do |host|
+  if fact_on(host, 'os.family') == %r{freebsd}
+    install_package(host, 'ruby-gems')
     %w[ruby irb gem].each do |executable|
       on host, "ln -sf /usr/local/bin/#{executable}#{version} /usr/local/bin/#{executable}"
     end
   end
+
 end
 
-# Install Puppet on all hosts
-install_puppet_agent_on(hosts, options) unless ENV['BEAKER_provision'] == 'no'
+# options = {
+#   default_action: 'gem_install'
+# }
 
-RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+# block_on hosts, run_in_parallel: false do |host|
+#   on host, 'pkg install -y devel/ruby-gems' if host['platform'] =~ %r{freebsd}
+#   if host['platform'] =~ %r{openbsd}
+#     version = ''
+#     if host['platform'] =~ %r{openbsd-6}
+#       on host, '. /root/.profile && pkg_add ruby-2.3.1p1'
+#       version = '23'
+#     end
 
-  c.formatter = :documentation
+#     %w[ruby irb gem].each do |executable|
+#       on host, "ln -sf /usr/local/bin/#{executable}#{version} /usr/local/bin/#{executable}"
+#     end
+#   end
+# end
 
-  c.before :suite do
-    # Install module to all hosts
-    hosts.each do |host|
-      install_dev_puppet_module_on(host, source: module_root, module_name: 'hugo')
-      # Install dependencies
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0]
-      on host, puppet('module', 'install', 'lwf-remote_file'), acceptable_exit_codes: [0]
-    end
-  end
-end
+# # Install Puppet on all hosts
+# install_puppet_agent_on(hosts, options) unless ENV['BEAKER_provision'] == 'no'
+
+# RSpec.configure do |c|
+#   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+#   c.formatter = :documentation
+
+#   c.before :suite do
+#     # Install module to all hosts
+#     hosts.each do |host|
+#       install_dev_puppet_module_on(host, source: module_root, module_name: 'hugo')
+#       # Install dependencies
+#       on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0]
+#       on host, puppet('module', 'install', 'lwf-remote_file'), acceptable_exit_codes: [0]
+#     end
+#   end
+# end
