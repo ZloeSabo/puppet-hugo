132706131c6ad4bd6af945f922c27df3e94c3f01
Evgeny Soynov <saboteur@saboteur.me>
2020-08-27 10:20:56 +0200

    More fixes

--- examples/with_repo.pp
+++ examples/with_repo.pp
@@ -5,24 +5,24 @@ $revision = 'master'
 # $revision = 'e842ba8cdeac6e2bacc9c58697b4e80bab10c26e'
 
 class {'::hugo':
-    manage_dependencies => true,
-    dependencies_ensure => 'latest',
-    dependencies => ['tar'],
-    manage_package => true,
-    package_ensure => 'present',
+    manage_dependencies    => true,
+    dependencies_ensure    => 'latest',
+    dependencies           => ['tar'],
+    manage_package         => true,
+    package_ensure         => 'present',
     installation_directory => '/usr/local/bin',
-    version => '0.20.7',
-    owner => 'root',
-    group => 'root',
-    mode => 'ug=rw,o=r,a=x',
+    version                => '0.20.7',
+    owner                  => 'root',
+    group                  => 'root',
+    mode                   => 'ug=rw,o=r,a=x',
     sites                  => {
-        $repo_path => {
-            'target' => '/tmp/test.org',
+        $repo_path  => {
+            'target'  => '/tmp/test.org',
             'version' => $revision
         },
         'test2.org' => {
-            'source' => '/tmp/test',
-            'target' => '/tmp/test2.org',
+            'source'  => '/tmp/test',
+            'target'  => '/tmp/test2.org',
             'version' => $revision
         }
     },
